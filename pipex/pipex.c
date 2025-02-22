fd31e7b99ec374bcd556da5bbb553be48c46f191
k0ff33blu3 <milva.ricci@gmail.com>
2025-02-22 14:31:53 +0100

    pipex.c pipex_utils.c norminettati

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,39 +1,8 @@
 #include "pipex.h"
-#include <errno.h>
-
-static void	close_all(t_pipex pipex)
-{
-	close(pipex.in_fd);
-	close(pipex.out_fd);
-	close(pipex.pipe[0]);
-	close(pipex.pipe[1]);
-}
-
-static void	cmd_not_found(t_pipex pipex)
-{
-	ft_putstr_fd(pipex.cmd, STDERR_FILENO);
-	ft_putstr_fd(": command not found\n", STDERR_FILENO);
-	ft_free(pipex.cmd_args, -1);
-	free(pipex.cmd_path);
-	free(pipex.cmd);
-	exit(127);
-}
-
-void	ft_error(char *str)
-{
-	perror(str);
-	exit(EXIT_FAILURE);
-}
 
 static void	first_child(t_pipex pipex, char *cmd, char *infile, char **envp)
 {
-	pipex.cmd_args = str_split(cmd, ' ');
-	pipex.cmd = ft_strdup(pipex.cmd_args[0]);
-	pipex.cmd_path = find_cmd_path(pipex.cmd_args[0], envp);
-	if (!pipex.cmd_path)
-		cmd_not_found(pipex);
-	free(pipex.cmd_args[0]);
-	pipex.cmd_args[0] = ft_strdup(pipex.cmd_path);
+	parse_cmd(&pipex, cmd, envp);
 	if (access(infile, R_OK) == -1)
 	{
 		ft_free(pipex.cmd_args, -1);
@@ -44,88 +13,63 @@ static void	first_child(t_pipex pipex, char *cmd, char *infile, char **envp)
 	dup2(pipex.in_fd, STDIN_FILENO);
 	dup2(pipex.pipe[1], STDOUT_FILENO);
 	close_all(pipex);
-	if (access(pipex.cmd_path, X_OK) != -1)
-	{
-		if (execve(pipex.cmd_path, pipex.cmd_args, envp) < 0)
-		{
-			ft_free(pipex.cmd_args, -1);
-			free(pipex.cmd);
-			free(pipex.cmd_path);
-			ft_error("execve");
-		}
-	}
+	exec_command(pipex, envp);
 }
-
 
 static void	last_child(t_pipex pipex, char *cmd, char *outfile, char **envp)
 {
-	pipex.cmd_args = str_split(cmd, ' ');
-	pipex.cmd = ft_strdup(pipex.cmd_args[0]);
-	pipex.cmd_path = find_cmd_path(pipex.cmd_args[0], envp);
-	if (!pipex.cmd_path)
-		cmd_not_found(pipex);
-	free(pipex.cmd_args[0]);
-	pipex.cmd_args[0] = ft_strdup(pipex.cmd_path);
+	parse_cmd(&pipex, cmd, envp);
 	dup2(pipex.pipe[0], STDIN_FILENO);
 	if (access(outfile, W_OK) == -1)
 	{
 		ft_free(pipex.cmd_args, -1);
-		free(pipex.cmd_path);
 		free(pipex.cmd);
+		free(pipex.cmd_path);
 		ft_error(outfile);
 	}
 	dup2(pipex.out_fd, STDOUT_FILENO);
 	close_all(pipex);
-	if (access(pipex.cmd_path, X_OK) != -1)
-	{
-		if (execve(pipex.cmd_path, pipex.cmd_args, envp) < 0)
-		{
-			ft_free(pipex.cmd_args, -1);
-			free(pipex.cmd_path);
-			free(pipex.cmd);
-			ft_error("execve");
-		}
-	}
+	exec_command(pipex, envp);
 }
 
+static void	run_pipex(t_pipex pipex, char **argv, char **envp)
+{
+	pid_t	pid1;
+	pid_t	pid2;
+	int		status1;
+	int		status2;
+
+	if (pipe(pipex.pipe) == -1)
+		perror("qualcosa");
+	pipex.in_fd = open(argv[1], O_RDONLY);
+	pipex.out_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	pid1 = fork();
+	if (pid1 < 0)
+		ft_error("first fork");
+	if (pid1 == 0)
+		first_child(pipex, argv[2], argv[1], envp);
+	pid2 = fork();
+	if (pid2 < 0)
+		ft_error("second fork");
+	if (pid2 == 0)
+		last_child(pipex, argv[3], argv[4], envp);
+	close_all(pipex);
+	waitpid(pid1, &status1, 0);
+	waitpid(pid2, &status2, 0);
+	if (WIFEXITED(status2) && WEXITSTATUS(status2))
+		exit(WEXITSTATUS(status2));
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_pipex		pipex;
-	pid_t		pid1;
-	pid_t		pid2;
-	int			status1;
-	int			status2;
+	t_pipex	pipex;
 
 	if (argc == 5)
-	{
-		if (pipe(pipex.pipe) == -1)
-			perror("qualcosa");
-		pipex.in_fd = open(argv[1], O_RDONLY);
-		pipex.out_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-		pid1 = fork();
-		if (pid1 < 0)
-			ft_error("first fork");
-		if (pid1 == 0)
-			first_child(pipex, argv[2], argv[1], envp);
-		pid2 = fork();
-		if (pid2 < 0)
-			ft_error("second fork");
-		if (pid2 == 0)
-			last_child(pipex, argv[3], argv[4], envp);
-		close_all(pipex);
-		waitpid(pid1, &status1, 0);
-		waitpid(pid2, &status2, 0);
-		if (WIFEXITED(status2))
-		{
-			if (WEXITSTATUS(status2))
-				exit(WEXITSTATUS(status2));
-		}
-	}
+		run_pipex(pipex, argv, envp);
 	else
 	{
-		ft_putstr_fd("Wrong argument number", STDERR_FILENO);
-		ft_putstr_fd("Inserire 4 argomenti", STDOUT_FILENO);
+		ft_putstr_fd("Wrong argument number\n", STDERR_FILENO);
+		ft_putstr_fd("Inserire 4 argomenti\n", STDOUT_FILENO);
 		exit(EXIT_FAILURE);
 	}
 	return (0);
---
+++ pipex/pipex_utils.c
@@ -0,0 +1,50 @@
+#include "pipex.h"
+
+void	close_all(t_pipex pipex)
+{
+	close(pipex.in_fd);
+	close(pipex.out_fd);
+	close(pipex.pipe[0]);
+	close(pipex.pipe[1]);
+}
+
+void	cmd_not_found(t_pipex pipex)
+{
+	ft_putstr_fd(pipex.cmd, STDERR_FILENO);
+	ft_putstr_fd(": command not found\n", STDERR_FILENO);
+	ft_free(pipex.cmd_args, -1);
+	free(pipex.cmd_path);
+	free(pipex.cmd);
+	exit(127);
+}
+
+void	ft_error(char *str)
+{
+	perror(str);
+	exit(EXIT_FAILURE);
+}
+
+void	parse_cmd(t_pipex *pipex, char *cmd, char **envp)
+{
+	pipex->cmd_args = str_split(cmd, ' ');
+	pipex->cmd = ft_strdup(pipex->cmd_args[0]);
+	pipex->cmd_path = find_cmd_path(pipex->cmd_args[0], envp);
+	if (!pipex->cmd_path)
+		cmd_not_found(*pipex);
+	free(pipex->cmd_args[0]);
+	pipex->cmd_args[0] = ft_strdup(pipex->cmd_path);
+}
+
+void	exec_command(t_pipex pipex, char **envp)
+{
+	if (access(pipex.cmd_path, X_OK) != -1)
+	{
+		if (execve(pipex.cmd_path, pipex.cmd_args, envp) < 0)
+		{
+			ft_free(pipex.cmd_args, -1);
+			free(pipex.cmd);
+			free(pipex.cmd_path);
+			ft_error("execve");
+		}
+	}
+}
