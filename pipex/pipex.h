0eafbf5f486ce696bf6d358297a7d87139c9d360
k0ff33blu3 <milva.ricci@gmail.com>
2025-02-22 15:50:03 +0100

    norminettato

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -9,17 +9,23 @@
 # include <errno.h>
 # include "libft/libft.h"
 
-char	*find_cmd_path(char *cmd, char **envp);
-char	**str_split(char *s, char c);
-
 typedef struct s_pipex
 {
-	int	in_fd;
-	int	out_fd;
+	int		in_fd;
+	int		out_fd;
+	int		pipe[2];
 	char	*cmd;
 	char	*cmd_path;
 	char	**cmd_args;
-	int	pipe[2];
-} t_pipex;
+}	t_pipex;
+
+char	*find_cmd_path(char *cmd, char **envp);
+char	**str_split(char *s, char c);
+char	**fill_split(char **dest, const char *s, char c, int words);
+void	close_all(t_pipex pipex);
+void	cmd_not_found(t_pipex pipex);
+void	ft_error(char *str);
+void	parse_cmd(t_pipex *pipex, char *cmd, char **envp);
+void	exec_command(t_pipex pipex, char **envp);
 
 #endif
--- pipex/str_split.c
+++ pipex/str_split.c
@@ -1,93 +1,45 @@
 #include "pipex.h"
 
-static int	countwords(char *s, char c)
+static int	skip_quote(char *s, int i)
 {
-	int	r;
-	int	in_word;
-	int	i;
 	char	quote;
 
-	r = 0;
+	quote = s[i];
+	i++;
+	while (s[i] && s[i] != quote)
+		i++;
+	if (s[i] == quote)
+		i++;
+	return (i);
+}
+
+static int	countwords(char *s, char c)
+{
+	int	count;
+	int	in_word;
+	int	i;
+
+	count = 0;
 	in_word = 0;
 	i = 0;
 	while (s[i])
 	{
 		if ((s[i] == 39 || s[i] == 34) && !in_word)
 		{
-			quote = s[i];
-			i++;
 			in_word = 1;
-			r++;
-			while (s[i] && s[i] != quote)
-				i++;
-			if (s[i] == quote)
-				i++;
+			count++;
+			i = skip_quote(s, i);
 		}
 		if (s[i] != c && in_word == 0)
 		{
-			r++;
+			count++;
 			in_word = 1;
 		}
 		else if (s[i] == c)
 			in_word = 0;
 		i++;
 	}
-	return (r);
-}
-
-static int	word_len(const char *s, unsigned int start, char end)
-{
-	int	i;
-
-	i = 0;
-	while (s[start] && s[start] != end)
-	{
-		i++;
-		start++;
-	}
-	return (i);
-}
-
-static char	**fill_split(char **dest, const char *s, char c, int words)
-{
-	int	i;
-	int	j;
-	int	k;
-	char	quote;
-
-	i = 0;
-	j = 0;
-	while (i < words)
-	{
-		while (s[j] == c)
-			j++;
-		if (s[j] == 39 || s[j] == 34)
-		{
-			quote = s[j];
-			j++;
-			dest[i] = (char *)malloc(sizeof(char) * (word_len(s, j, quote) + 1));
-			if (!dest[i])
-				return (ft_free(dest, i), NULL);
-			k = 0;
-			while (s[j] && s[j] != quote)
-				dest[i][k++] = s[j++];
-			if (s[j] == quote)
-				j++;
-		}
-		else
-		{
-			dest[i] = (char *)malloc(sizeof(char) * (word_len(s, j, c) + 1));
-			if (!dest[i])
-				return (ft_free(dest, i), NULL);
-			k = 0;
-			while (s[j] && s[j] != c)
-				dest[i][k++] = s[j++];
-		}
-		dest[i][k] = '\0';
-		i++;
-	}
-	dest[i] = NULL;
-	return (dest);
+	return (count);
 }
 
 char	**str_split(char *s, char c)
@@ -103,21 +55,23 @@ char	**str_split(char *s, char c)
 		return (NULL);
 	return (fill_split(dest, s, c, words));
 }
-
 /*
-int main(int ac, char **av)
+int	main(int argc, char **argv)
 {
-	char **res;
-	int	i;
+	char **result;
+	int i;
 	
-	i = 0;
-	if (ac < 2)
-	return 1;
-	res = str_split(av[1], ' ');
-	while(res[i])
-	printf("%s\n", res[i++]);
-	ft_free(res, -1);
+	if (argc > 0)
+	{
+		result = str_split(argv[1], ' ');
+		i = 0;
+		while (i<3)
+		{
+			ft_putstr_fd(result[i], 1);
+			ft_putstr_fd("\n", 1);
+			i++;
+		}
+	}
 	return 0;
 }
 */
-
