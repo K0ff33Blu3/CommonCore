f8e8fb16e015c9c8b6eecdd327d16a403451b35e
Milva Ricci <miricci@c1r5p3.42firenze.it>
2025-02-27 18:03:17 +0100

    e anche oggi non ho fatto un cazzo

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,38 +1,9 @@
 #include "push_swap.h"
 
-t_list	**create_list(int stack_size, char **nptr)
-{
-	t_list	**list;
-	t_list	*node;
-	int	i;
-
-	i = 0;
-	*list = NULL;
-	while (i < stack_size)
-	{
-		node = ft_lstnew(ft_atoi(nptr[i]));
-		if (!node)
-		// liberare nodi precedenti
-			return (NULL);
-		ft_lstadd_back(list, node);
-		i++;
-	}
-	return (list);
-}
-
-void	print_list(t_list *list)
-{
-	while(list->next)
-	{
-		ft_putnbr_fd(list->content, STDOUT_FILENO);
-		ft_putchar_fd("\n", STDOUT_FILENO);
-		list->next;
-	}
-}
-
 int	main(int ac, char **av)
 {
-	create_list((ac - 2), av);
-	
+	t_list	*arg;
+	arg = *create_list((ac - 2), av++);
+	print_list(arg);
 	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -5,7 +5,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
-# include "libft/libft_bonus.h"
+
+typedef struct s_nlist
+{
+	int	nbr;
+	struct s_list *next;
+}	t_list;
 
 
 #endif
