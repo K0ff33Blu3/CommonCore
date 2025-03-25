6dcd365d58ae0e27e6e775cd0d5105152352f1d4
Milva Ricci <miricci@c1r5p4.42firenze.it>
2025-03-25 14:26:33 +0100

    cerchiamo di sincronizzare il pc di migrazione

--- pipex/pipex_bonus.c
+++ pipex/pipex_bonus.c
@@ -6,7 +6,11 @@
 /*   By: miricci <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 14:35:23 by miricci           #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/04/02 14:46:21 by miricci          ###   ########.fr       */
+=======
+/*   Updated: 2025/03/25 14:25:25 by miricci          ###   ########.fr       */
+>>>>>>> 08b73cd (non so cosa ho cambiato)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +67,18 @@ static void	run_pipex(t_pipex pipex, int argc, char **argv, char **envp)
 	int		i;
 
 	pipex.out_fd = open(argv[argc -1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+<<<<<<< HEAD
 	i = 0;
 	while (i < pipex.n_cmd - 1)
 		ft_fork(&pipex, pipex.all_cmds[i++], envp);
+=======
+	if (pipex.limiter)
+		i = 3;
+	else
+		i = 2;
+	while (i < argc - 2)
+		ft_fork(&pipex, argv[i++], envp);
+>>>>>>> 08b73cd (non so cosa ho cambiato)
 	pid2 = fork();
 	if (pid2 < 0)
 		ft_error("fork");
@@ -79,6 +92,48 @@ static void	run_pipex(t_pipex pipex, int argc, char **argv, char **envp)
 		exit(WEXITSTATUS(status2));
 }
 
+<<<<<<< HEAD
+=======
+void	here_doc(t_pipex pipex, char **argv)
+{
+	char	*line;
+
+	pipex.limiter = ft_strjoin(argv[2], "\n");
+	if (pipe(pipex.pipe) == -1)
+		ft_error("pipe");
+	line = get_next_line(STDIN_FILENO);
+	while (line)
+	{
+		if (!ft_strncmp(line, pipex.limiter, ft_strlen(pipex.limiter)))
+		{
+			free(line);
+			break ;
+		}
+		ft_putstr_fd(line, pipex.pipe[1]);
+		free(line);
+		line = get_next_line(STDIN_FILENO);
+	}
+	close(pipex.pipe[1]);
+}
+
+void	handle_input(t_pipex pipex, char **argv, int argc)
+{
+	if (argc > 5)
+	{
+		if (!ft_strncmp(argv[1], "here_doc", 8))
+			here_doc(pipex, argv);
+		else
+		{
+			pipex.in_fd = open(argv[1], O_RDONLY);
+			if (access(argv[1], R_OK) == -1)
+				ft_error(argv[1]);
+			dup2(pipex.in_fd, STDIN_FILENO);
+			close(pipex.in_fd);
+		}
+	}
+}
+
+>>>>>>> 08b73cd (non so cosa ho cambiato)
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	pipex;
@@ -88,12 +143,19 @@ int	main(int argc, char **argv, char **envp)
 	{
 		if (!ft_strncmp(argv[1], "here_doc", 8))
 		{
+<<<<<<< HEAD
 			pipex.limiter = ft_strjoin(argv[2], "\n");
 			here_doc(&pipex, argv, argc);
 			free(pipex.limiter);
 		}
 		else
 			input_doc(&pipex, argv, argc);
+=======
+			here_doc(pipex, argv);
+			argv = argv + 2;
+		}
+
+>>>>>>> 08b73cd (non so cosa ho cambiato)
 		run_pipex(pipex, argc, argv, envp);
 	}
 	else
