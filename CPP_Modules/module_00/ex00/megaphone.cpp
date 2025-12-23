c4224fa69029e8a397c70cf72d84575b01d5ca7e
miricci42 <miricci@student.42firenze.it>
2025-12-23 15:59:36 +0100

    siefjoi

---
+++ CPP_Modules/module_00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 12:33:29 by miricci           #+#    #+#             */
+/*   Updated: 2025/12/23 15:58:28 by miricci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+// using namespace std;
+
+int	main(int argc, char **argv) {
+	// controllo sul numero di argomenti
+	if (argc != 2) {
+		std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 1;
+	}
+	// assegnazione di dell'argomento a s
+	std::string s = argv[1];
+	// utilizzo un ciclo perche il metodo toupper() appartiene alla classe char 
+		
+	for (std::string::iterator it = s.begin(); it != s.end(); ++it) {
+        	*it = std::toupper(*it);
+	}
+	std::cout << s << std::endl;
+	return 0;
+}
